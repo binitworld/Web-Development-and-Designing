25b176e5089c12345f82adb39a6bff0712243c3f
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-02-04 08:00:55 +0530

    Portfolio Web-Page

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Full-Stack Developer", "Dynamic Coder", "DevOps Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+function openGoogleSearch() {
+    window.location.href = 'https://www.google.com/search?q=binit+bhushan&oq=BINIT+B&gs_lcrp=EgZjaHJvbWUqCAgAEEUYJxg7MggIABBFGCcYOzIGCAEQRRg5MgYIAhAjGCcyBwgDEAAYgAQyBggEEEUYQTIGCAUQRRg8MgYIBhBFGDwyBggHEEUYQagCALACAA&sourceid=chrome&ie=UTF-8';
+}
